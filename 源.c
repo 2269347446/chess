389040d308015b423d64808ee4e734948c51a3c5
2269347446 <84495238+2269347446@users.noreply.github.com>
2021-06-02 13:51:49 +0800

    Update 源.c

--- 源.c
+++ 源.c
@@ -4,35 +4,26 @@
 
 int main()
 {
-	int bd[0][2];
-		int a;int b;
-		for (a = 0;a < 3;a++)
-		{
-			for (b = 0;b < 3;b++)
-			{
-				bd[a][b] = 55;
-			}
-		}
 	
-	//int input;
-	//do {
-	//	menu();
-	//	printf("\n是否开始游戏>>");
-	//	scanf("%d", &input);
-	//	system("cls");
-	//	switch(input)
-	//	{
-	//	case 1:
-	//		printf("play game\n");
-	//		game();
-	//		break;
-	//	case 0:
-	//		printf("out game\n");
-	//		break;
-	//	default:
-	//		printf("erroc\n");
-	//		break;
-	//	}
-	//} while (input);
+	int input;
+	do {
+		menu();
+		printf("\n脢脟路帽驴陋脢录脫脦脧路>>");
+		scanf("%d", &input);
+		system("cls");
+		switch(input)
+		{
+		case 1:
+			printf("play game\n");
+			game();
+			break;
+		case 0:
+			printf("out game\n");
+			break;
+		default:
+			printf("erroc\n");
+			break;
+		}
+	} while (input);
 	return 0;
 }
